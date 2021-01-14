d491c386402609e1ee6460cd5667bd438e1e4d82
Elton Lac <lacelton@yahoo.com>
2021-01-13 18:25:20 -0800

    Save button works

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+//Current day and time
 var d = new Date();
 document.getElementById("currentDay").innerHTML = d;
 
+//Chnge based on time of day
+var n = d.getHours();
+console.log(n);
+
+//Save button
 $(".saveBtn").click(function () {
-  console.log("Test");
-  console.log(window);
-  console.log(localStorage);
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, value);
 });
 
-$(".checkBtn").click(function () {
-  $(".description").each(function () {
+//Local storage information
+$(document).ready(function () {
+  $("#09 .description").val(localStorage.getItem("09"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
+});
+
+//Check button
+$(".description").each(function () {
+  $(".checkBtn").click(function () {
     if ($(".textarea").hasClass("completed")) {
       $(".textarea").removeClass("completed");
     } else {
